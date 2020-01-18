46b42de5463d30565c0903452fe327aa5226ac8c
Lukas Jenks <lukasjenks@gmail.com>
2020-01-18 15:02:28 -0700

    Add basic DB migration scripts

---
+++ Backend/migrations/01_init.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+
+CREATE TABLE store (
+	id integer primary key,
+	name text,
+	address text
+);
+
+-- + goose Down
+-- SQL in this section is executed when the migrations is rolled back.
+
+DROP TABLE store;
